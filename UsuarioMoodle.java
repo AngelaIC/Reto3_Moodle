8baa299507151f197229ffbcd7fbe2edc226e05d
AIC <104699297+AngelaIC@users.noreply.github.com>
2022-07-25 12:43:06 -0500

    Create UsuarioMoodle.java

---
+++ UsuarioMoodle.java
@@ -0,0 +1,67 @@
+public class UsuarioMoodle {
+    //Atributos
+    private String nombreCompleto;
+    private String documentoIdentidad;
+    private String password;
+    private String correo;
+    private String descripcion;
+    
+    //Constructor
+    public UsuarioMoodle(String nombreCompleto, String documentoIdentidad, String password, String correo, String descripcion) {
+        this.nombreCompleto = nombreCompleto;
+        this.documentoIdentidad = documentoIdentidad;
+        this.password = password;
+        this.correo = correo;
+        this.descripcion = descripcion;
+    }
+    
+    //Método
+    public void editarPerfil(String nombreCompleto, String documentoIdentidad, String password, String correo, String descripcion) {
+        this.nombreCompleto = nombreCompleto;
+        this.documentoIdentidad = documentoIdentidad;
+        this.password = password;
+        this.correo = correo;
+        this.descripcion = descripcion;
+    }
+    
+    //GETTERS y SETTERS
+    public String getNombreCompleto() {
+        return this.nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getDocumentoIdentidad() {
+        return this.documentoIdentidad;
+    }
+
+    public void setDocumentoIdentidad(String documentoIdentidad) {
+        this.documentoIdentidad = documentoIdentidad;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCorreo() {
+        return this.correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
